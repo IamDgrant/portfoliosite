489e3f3aab7aa8381a77348eb739950b9a511fcb
Andre Grant <Iamdgrant@me.com>
2021-04-29 11:12:41 -0400

    added portfolio site

---
+++ study.js
@@ -0,0 +1,16 @@
+// function avgOfFour(num1, num2, num3, num4) {
+//     let sum = num1 + num2 + num3 + num4;
+//     let avg = sum / 4;
+//     return avg;
+// }
+
+// console.log(avgOfFour(10, 10, 10, 10))
+
+let str = "Hi my name is Andre";
+
+const splitter = (str) => {
+    let splitUp = str.split(" ", 3);
+    return splitUp
+}
+
+console.log(splitter(str));
