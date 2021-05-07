3ab28b75d0cc4736c5b9817dea34f59887eac3a5
Andre Grant <Iamdgrant@me.com>
2021-05-07 01:28:54 -0400

    added down button with blinking css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import Logo from "./Logo";
 import Profile from "./Profile";
+import downBtn from "../assets/angle-double-down-solid.svg";
+import upBtn from "../assets/angle-double-up-solid.svg";
 import "../index.css";
 
 const Home = () => {
@@ -15,8 +17,25 @@ const Home = () => {
       <div className="main-container">
         <div className="main-container-logo">
           <Logo />
-          <div className="scroll-to-btn">
-            <button onClick={handleScrollClick}>SCROLL</button>
+          <div className="scroll-to-btn-container">
+            <button
+              onClick={handleScrollClick}
+              style={{
+                cursor: "pointer",
+                padding: "0",
+                border: "none",
+                background: "none",
+              }}
+            >
+              <img
+                src={downBtn}
+                alt="down arrow"
+                style={{
+                  width: "40px",
+                  height: "40px",
+                }}
+              />
+            </button>
           </div>
         </div>
 
