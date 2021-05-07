28b4ac290dfb20ba6ce747f894bc8a3b1710d137
Andre Grant <Iamdgrant@me.com>
2021-05-07 01:14:12 -0400

    added scroll to element

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
+import Logo from "./Logo";
 import Profile from "./Profile";
 import "../index.css";
 
 const Home = () => {
+  const profileRef = useRef();
+
+  const handleScrollClick = () => {
+    profileRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="main-container">
-        <div className="main-container-profile">
+        <div className="main-container-logo">
+          <Logo />
+          <div className="scroll-to-btn">
+            <button onClick={handleScrollClick}>SCROLL</button>
+          </div>
+        </div>
+
+        <div className="main-container-profile" ref={profileRef}>
           <Profile />
         </div>
       </div>
